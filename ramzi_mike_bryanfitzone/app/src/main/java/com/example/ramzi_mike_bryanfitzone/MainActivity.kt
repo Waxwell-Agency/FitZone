67dd33f2831758436f2cb565391b252e45254c20
Bryan Chedjou <chedjouarsene@gmail.com>
2026-07-31 23:09:18 -0400

    debut

---
+++ ramzi_mike_bryanfitzone/app/src/main/java/com/example/ramzi_mike_bryanfitzone/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ramzi_mike_bryanfitzone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ramzi_mike_bryanfitzone.ui.theme.Ramzi_mike_bryanfitzoneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ramzi_mike_bryanfitzoneTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ramzi_mike_bryanfitzoneTheme {
+        Greeting("Android")
+    }
+}
